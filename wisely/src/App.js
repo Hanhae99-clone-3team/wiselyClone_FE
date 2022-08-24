14262c23a971d894a1d09d104a20be7a35dd4248
rudans987 <rudans987@naver.com>
2022-08-25 00:04:56 +0900

    react helmet 적용

--- wisely/src/App.js
+++ wisely/src/App.js
@@ -1,12 +1,18 @@
 import GlobalRouter from "./global/GlobalRouter";
 import Globalstyle from"./global/Globalstyle";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+       <Helmet>
+        <title>와이즐리</title>
+        <link rel="icon" href="https://www.wiselycompany.com/favicon.ico" />
+      
+      </Helmet>
      <GlobalRouter/>
      <Globalstyle/>
-     {/* <Footer/> */}
+    
     </div>
   );
 }
